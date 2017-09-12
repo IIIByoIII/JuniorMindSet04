9e45b6f72d747ad5000cdcfa5dadb3f8cac82bec
IIIByoIII <robertkiss26@gmail.com>
2017-09-12 19:28:00 +0300

    Added CreateCharList test

--- Password/PasswordTests.cs
+++ Password/PasswordTests.cs
@@ -7,8 +7,23 @@ namespace Password
         public class PasswordTests
         {
                 [TestMethod]
-                public void TestMethod1()
+                public void CreateCharList() // {{{
                 {
-                }
+                        CollectionAssert.AreEqual(new char[] { 'a', 'b' ,'c' }, CharArray('a', 'c'));
+                } // }}}
+
+                char[] CharArray(char start, char end) // {{{ 
+                {
+                        if (start <= end) {
+                                char jChar = start;
+                                char[] result = new char[end + 1 - start];
+                                for (int i = 0; i < (end + 1 - start); i++) {
+                                        result[i] = jChar;
+                                        jChar++;
+                                }
+                                return result;
+                        }
+                        return new char[] {start};
+                } // }}}
         }
 }
