98e392da5e739ba9bcbb33be4f3f1e6deb7c6011
IIIByoIII <robertkiss26@gmail.com>
2017-09-19 21:04:38 +0300

    Added SecondItemPrice test

--- Shopping/ShoppingTests.cs
+++ Shopping/ShoppingTests.cs
@@ -7,17 +7,24 @@ namespace Shopping
     public class ShoppingTests
     {
         [TestMethod]
-        public void FirstItemName()
+        public void FirstItemName()/*fs*/
         {
             Assert.AreEqual("apple", A.name);
-        }
+        }/*fe*/
+
+        [TestMethod]
+        public void SecondItemPrice()/*fs*/
+        {
+            Assert.AreEqual(3.8, B.price);
+        }/*fe*/
 
         Item A = new Item { name = "apple", price = 3.2 };
+        Item B = new Item { name = "pear", price = 3.8 };
 
-        struct Item
+        struct Item/*fs*/
         {
             public string name;
             public double price;
-        }
+        }/*fe*/
     }
 }
