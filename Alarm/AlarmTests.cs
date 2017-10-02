2227439e2f5c450229cf0bd66ca834e0492a485a
IIIByoIII <robertkiss26@gmail.com>
2017-10-02 20:47:24 +0300

    Added GetMondayAlarm test
    Modified start counter of Day enum also.

--- Alarm/AlarmTests.cs
+++ Alarm/AlarmTests.cs
@@ -9,18 +9,34 @@ namespace Alarm
         [TestMethod]
         public void GetFriday()
         {
-            Assert.AreEqual(5, (int)Day.Friday);
+            Assert.AreEqual(4, (int)Day.Friday);
         }
 
-        enum Day
+        public enum Day
         {
-            Monday = 1,
+            Monday,
             Tuesday,
             Wednesday,
             Thursday,
             Friday,
             Saturday,
             Sunday
+        }
+
+        [TestMethod]
+        public void GetMondayAlarm()
+        {
+            string[] alarmTimes = SetAlarmTimes(new string[7], "06:00", (byte)Day.Monday);
+            Assert.AreEqual("06:00", alarmTimes[(byte)Day.Monday]);
+        }
+
+        string[] SetAlarmTimes(string[] prevTimes, string alarmTime, byte startDay, byte endDay = 0)
+        {
+            if (startDay > endDay)
+                endDay = startDay;
+            for (int i = startDay; i <= endDay; i++)
+                prevTimes[i] = alarmTime;
+            return prevTimes;
         }
     }
 }
