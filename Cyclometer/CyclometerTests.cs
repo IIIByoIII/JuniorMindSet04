9764e5d42391722f26d536c6525cd078804d8f8e
IIIByoIII <robertkiss26@gmail.com>
2017-10-07 17:15:41 +0300

    Added GetTotalDistance test

--- Cyclometer/CyclometerTests.cs
+++ Cyclometer/CyclometerTests.cs
@@ -3,12 +3,46 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Cyclometer
 {
+    public struct Cyclist
+    {
+        public string name;
+        public double diameter;
+        public int[] revSec;
+        public Cyclist(string name, double diameter, int[] revSec) {
+            this.name = name;
+            this.diameter = diameter;
+            this.revSec = revSec;
+        }
+    }
+
     [TestClass]
     public class CyclometerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetTotalDistance()
         {
+            var cyclists = new Cyclist[] { 
+                new Cyclist("Radu", 26.4, new int[] {2, 2, 3, 3, 4, 4}),
+                new Cyclist("Alin", 24.0, new int[] {3, 3, 4, 4, 3, 3}),
+                new Cyclist("Robi", 27.0, new int[] {1, 1, 2, 3, 4, 5})
+            }; // total 18+20+16=54 revolutions
+            Assert.AreEqual(((18*26.4)+(20*24.0)+(16*27.0))*Math.PI, TotalDistance(cyclists));
+        }
+
+        double TotalDistance(Cyclist[] cyclists)
+        {
+            double totalDistance = 0;
+            double cyclistRevolutions;
+            for (int i = 0; i < cyclists.Length; i++) {
+                cyclistRevolutions = 0;
+                for (int j = 0; j < cyclists[i].revSec.Length; j++)
+                    cyclistRevolutions += cyclists[i].revSec[j];
+                totalDistance += cyclistRevolutions * cyclists[i].diameter;
+            }
+            totalDistance *= Math.PI;
+            return totalDistance;
         }
     }
 }
+
+
