790bdccec6a10ec5bf3e83cd0dcc3544db5cca98
IIIByoIII <robertkiss26@gmail.com>
2017-10-11 18:41:00 +0300

    Added new StringToCoordinates test

--- Intersection/IntersectionTests.cs
+++ Intersection/IntersectionTests.cs
@@ -1,14 +1,59 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Intersection
 {
+    enum Vector {R, U, L, D};
+
+    struct Coord
+    {
+        public int? x, y;
+
+        public Coord(int? x, int? y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     [TestClass]
     public class IntersectionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void StringToCoordinates()
         {
+            CollectionAssert.AreEqual(new List<Coord> { new Coord(0, 0), new Coord(1, 0), new Coord(1, 1) }, GetCoords("RU"));
         }
+
+        List<Coord> GetCoords(string vectors)
+        {
+            int x = 0;
+            int y = 0;
+            string direction;
+            var result = new List<Coord>();
+            result.Add(new Coord(x, y));
+            for (int i = 0; i < vectors.Length; i++) {
+                direction = vectors[i].ToString();
+                Vector vector = (Vector)Enum.Parse(typeof(Vector), direction);
+                switch ((int)vector)
+                {
+                    case 0:
+                        x++;
+                        break;
+                    case 1:
+                        y++;
+                        break;
+                    case 2:
+                        x--;
+                        break;
+                    default:
+                        y--;
+                        break;
+                }
+                result.Add(new Coord(x, y));
+            }
+            return result;
+        }
+
     }
 }
